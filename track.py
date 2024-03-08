13de97e9cb305ef2b155dffdcea40571e095d431
Jesus <jesushdz2002ch14@gmail.com>
2024-03-08 14:49:09 -0600

    added 2nd serial port

--- track.py
+++ track.py
@@ -4,31 +4,37 @@ import os
 import time
 
 # to give jetson permission for Arduino USB port
-os.system('echo mule123 | sudo -S chmod a+rw /dev/ttyUSB0')
+os.system('echo mule123 | sudo -S chmod a+rw /dev/ttyUSB0')  # arduino for cameras 
+os.system('echo mule123 | sudo -S chmod a+rw /dev/ttyACM0')  # arduino for motors 
 
 def drawBox(img, bbox):
     x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
-    print(bbox)
+    # print(bbox)
     cv2.rectangle(img, (x, y), ((x+w), (y+h)), (255, 0, 255), 3, 1)
     cv2.putText(img, "Tracking", (75, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
-    send_data(bbox)
+    send_data(f"{x}, {y}, {w}, {h}")
     
 def send_data(data):
     '''Convert the 4-tuple into a comma-sep string'''
-    data_str = ','.join(map(str, data)) + '\n'  # Convert tuple to comma-separated string
-    ser.write(data_str.encode())  # Send data as bytes
+    data_str = data + '\n'  # Convert tuple to comma-separated string
+    print(data)
+    ser.write(data_str.encode())  # Send data to camera mov. arduino
+    ser2.write(data_str.encode())  # Send data to motor mov. arduino
    
-# open serial port
+# open serial port for camera movement
 ser = serial.Serial('/dev/ttyUSB0', 9600)
+# open serial port for motor movement
+ser2 = serial.Serial('/dev/ttyACM0', 9600)
+
 # reset camera to default pos. before capture
-time.sleep(4)
+time.sleep(2)
 # keep width and height at 640, 480, for faster response
 cap = cv2.VideoCapture('''nvarguscamerasrc ! video/x-raw(memory:NVMM), 
 width=640, height=480, format=(string)NV12, framerate=(fraction)21/1 
 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw,
  format=(string)BGR ! appsink''', cv2.CAP_GSTREAMER)
  # MOSSE is fast, but less accurate
-tracker = cv2.legacy.TrackerMOSSE_create()
+tracker = cv2.legacy.TrackerMedianFlow_create()
 # csrt is more accurate
 # tracker = cv2.legacy.TrackerCSRT_create()
 success, img = cap.read()
